91df993baf051dacae1e71a60f16bc783f6974f5
Ayush <ayush.singh18@outlook.com>
2023-07-03 16:16:18 +0530

    Added logos in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import styles from '@/styles/Footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
     return (
@@ -12,13 +13,13 @@ const Footer = () => {
                     </div>
                     <div className={styles.rowRight}>
                         <a href='https://github.com/Ayush181005' className={styles.footerIcon} target='_blank' rel="noreferrer noopener" title='Github'>
-                            {/* <FontAwesomeIcon icon="fa-brands fa-github" style={{color:'#211F1F'}} /> */}
+                            <Image src='/icons/github.svg' width={20} height={20} alt='Github icon' />
                             &nbsp;
                             <span className={styles.text}>Github</span>
                         </a>
 
                         <a href='https://stackoverflow.com/users/15543100/ayush?tab=profile' className={styles.footerIcon} target='_blank' rel="noreferrer noopener" title='StackOverflow'>
-                            {/* <FontAwesomeIcon icon="fa-brands fa-stack-overflow" style={{color:'#EF8236'}} /> */}
+                            <Image src='/icons/stack-overflow.svg' width={20} height={20} alt='StackOverflow icon' />
                             &nbsp;
                             <span className={styles.text}>StackOverflow</span>
                         </a>
