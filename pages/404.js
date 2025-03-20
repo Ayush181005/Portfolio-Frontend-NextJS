62a4da6b4b08eb1ba7a7ea9fe8975b56084fc9e6
Ayush Singh <ayush.singh18@outlook.com>
2025-03-20 22:10:25 +0530

    Added custom 404

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+export default function Custom404() {
+    return (
+        <>
+            <div style={{textAlign: 'center', padding: '10rem 0', fontFamily: 'Arial, sans-serif' }}>
+                <h1>404 - Page Not Found</h1>
+                <p>Oops! The page you are looking for does not exist.</p>
+            </div>
+        </>
+    );
+}
